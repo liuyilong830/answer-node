95f81e6f650217092662f0ea0d0cb59322deea81
liuyilong <980743424@qq.com>
2021-01-23 23:20:41 +0800

    完成了发布评论和删除评论的逻辑

--- db/views/comments.js
+++ db/views/comments.js
@@ -1,4 +1,4 @@
-const { queryFunc } = require('../../utils')
+const { queryFunc, jointoStr } = require('../../utils')
 
 const comments = {
   getQuesCommentList(qid, start = 0, limit = 10) {
@@ -16,6 +16,63 @@ const comments = {
       where c.fromid = ?
       order by c.createtime asc limit ${start}, ${limit}
     `;
+    return queryFunc(sql, cid);
+  },
+  queryCommentById(cid) {
+    let sql = `
+      select *
+      from comments c inner join user u on c.usid = u.uid
+      where c.cid = ?
+    `
+    return queryFunc(sql, cid);
+  },
+  createComment(uid, info) {
+    let keys = Object.keys(info).filter(key => info[key]);
+    let str = keys.join(',');
+    let values = "";
+    keys.forEach(key => {
+      let s = info[key]
+      if (typeof s === 'string') {
+        s = `'${s}'`
+      }
+      values += s + ',';
+    })
+    values = values.slice(0, values.length-1)
+    let sql = `
+      insert into comments(usid,${str}) 
+      values(${uid},${values})
+    `;
+    return queryFunc(sql);
+  },
+  updateCommentCount(cid, num = 0) {
+    let sql = `
+      update comments set count=count+${num}
+      where cid = ?
+    `;
+    return queryFunc(sql, cid);
+  },
+  deleteCommentByCid(cid) {
+    let sql = `
+      delete from comments
+      where cid = ?
+    `;
+    return queryFunc(sql, cid);
+  },
+  queryCommentByCidAndUid(cid, uid) {
+    let sql = `
+      select *
+      from comments
+      where cid = ? and usid = ?
+    `
+    return queryFunc(sql, cid, uid);
+  },
+  queryChildComment(cid) {
+    let sql = `
+      select *
+      from comments
+      where targetid = ?
+      order by createtime asc
+    `
     return queryFunc(sql, cid);
   },
 }
--- router/comments.js
+++ router/comments.js
@@ -2,10 +2,17 @@ const Router = require('@koa/router')
 const Comments = new Router({
   prefix: '/comments',
 })
+const moment = require('moment')
 const { tokenFailure, resBody, format, isdef, responseFormat } = require('../utils')
 const {
   getQuesCommentList,
   getAllReplyComments,
+  queryCommentById,
+  createComment,
+  deleteCommentByCid,
+  updateCommentCount,
+  queryCommentByCidAndUid,
+  queryChildComment,
 } = require('../db/views/comments')
 
 Comments.get('/queslist', async ctx => {
@@ -32,18 +39,110 @@ Comments.get('/allreply', async ctx => {
     });
   }
   let arr = responseFormat(await getAllReplyComments(cid, start, limit));
-  let res = arr.map(comment => {
-    comment.targetInfo = {};
-    if (comment.targetid !== cid) {
-      let index = arr.findIndex(com => com.cid === comment.targetid);
-      comment.targetInfo = arr[index];
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    let comment = arr[i];
+    comment.targetInfo = {}
+    if (comment.targetid) {
+      let [info] = responseFormat(await queryCommentById(comment.targetid));
+      comment.targetInfo = info;
     }
-    return comment;
-  })
+    res.push(comment);
+  }
 
   return resBody(ctx, {
     message: '查询成功',
     data: res,
+  })
+})
+
+Comments.post('/create', async ctx => {
+  if (!tokenFailure(ctx.token, ctx)) return;
+  let body = format(ctx.request.body);
+  let { uid } = ctx.info;
+  (['questionid', 'timuid', 'fromid', 'targetid']).forEach(key => {
+    if (!body[key]) {
+      body[key] = null
+    }
+  })
+  if (!body.questionid && !body.timuid) {
+    return resBody(ctx, {
+      status: 401,
+      message: `timuid和questionid是互斥且必须有其一的参数`
+    });
+  }
+  body.createtime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+  if (body.fromid && body.targetid) {
+    await updateCommentCount(body.fromid, 1)
+    if (body.targetid !== body.fromid) {
+      await updateCommentCount(body.targetid, 1)
+    }
+  }
+  let { insertId } = await createComment(uid, body);
+  let [res] = responseFormat(await queryCommentById(insertId));
+  res.targetInfo = {};
+  if (res.targetid) {
+    res.targetInfo = responseFormat(await queryCommentById(res.targetid))[0]
+  }
+  return resBody(ctx, {
+    message: '评论成功',
+    data: res,
+  })
+})
+
+Comments.delete('/delete', async ctx => {
+  if (!tokenFailure(ctx.token, ctx)) return;
+  let { uid } = ctx.info;
+  let { cid } = format(ctx.query);
+  if (!cid) {
+    return resBody(ctx, {
+      status: 401,
+      message: 'cid是必须的参数'
+    });
+  }
+  let [com] = (await queryCommentByCidAndUid(cid, uid));
+  if (!com) {
+    return resBody(ctx, {
+      message: '没有该评论'
+    });
+  }
+
+  let index = 0;
+  let stack = [];
+  function Temp(cid) {
+    this.cid = cid;
+    this.child = [];
+  }
+  let beforeid = null;
+  stack.push(new Temp(cid));
+  while (stack.length) {
+    let obj = stack.pop();
+    if (obj.child && obj.child[0] === beforeid) {
+      index++;
+      beforeid = obj.cid;
+      deleteCommentByCid(obj.cid);
+      continue;
+    }
+    let arr = await queryChildComment(obj.cid);
+    obj.child = arr.map(item => item.cid);
+    if (obj.child.length) {
+      stack.push(obj);
+      obj.child.forEach(cid => {
+        stack.push(new Temp(cid));
+      })
+    } else {
+      index++;
+      beforeid = obj.cid;
+      deleteCommentByCid(obj.cid);
+    }
+  }
+  if (com.fromid) {
+    updateCommentCount(com.fromid, -index);
+  }
+
+  return resBody(ctx, {
+    message: '删除成功',
+    count: index,
   })
 })
 
