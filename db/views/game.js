6d63a6630afe76bb4f825f1d153973aad19cff64
liuyilong <980743424@qq.com>
2021-02-13 21:35:10 +0800

    完成了更新挑战记录、以及给定在到达开始时间、禁止入场时间、结束时间进行对应挑战赛的定时处理方法

--- db/views/game.js
+++ db/views/game.js
@@ -33,10 +33,78 @@ const game = {
       select * 
       from rank_game r inner join challenge_record c on r.rankid = c.rankid
       where c.chuserid = ?
-      order by status desc limit ${start}, ${limit}
+      order by status asc limit ${start}, ${limit}
     `;
     return queryFunc(sql, uid);
-  }
+  },
+  queryAllGames(start = 0, limit = 10) {
+    let sql = `select * from rank_game limit ${start}, ${limit}`
+    return queryFunc(sql);
+  },
+  queryAppointmentOtherGames(uid, start = 0, limit = 10) {
+    let sql = `
+      select * 
+      from rank_game
+      where rankid not in (
+        select c.rankid
+        from challenge_record c inner join rank_game r on c.rankid = r.rankid
+        where c.chuserid = ?
+      )
+      order by starttime asc limit ${start}, ${limit}
+    `;
+    return queryFunc(sql, uid);
+  },
+  updateGameAppointment(rankid, num = 0) {
+    let sql = `
+      update rank_game set reservation_num = reservation_num + ${num}
+      where rankid = ?
+    `;
+    return queryFunc(sql, rankid);
+  },
+  insertChallengeRecord(rankid, uid) {
+    let sql = `
+      insert into challenge_record(rankid, chuserid)
+      values(${rankid}, ${uid})
+    `;
+    return queryFunc(sql);
+  },
+  deleteChallengeRecord(rankid, uid) {
+    let sql = `
+      delete from challenge_record where rankid = ${rankid} and chuserid = ${uid}
+    `;
+    return queryFunc(sql);
+  },
+  queryDoingGames(uid) {
+    let sql = `
+      select * 
+      from rank_game r inner join challenge_record c on r.rankid = c.rankid
+      where c.chuserid = ? and c.status = 1
+    `;
+    return queryFunc(sql, uid);
+  },
+  updateChallengeRecord(uid, challengeid, info) {
+    let str = '';
+    let keyStr = ['finishtime', 'json']
+    let keyint = ['time', 'finishtime', 'json', 'status'];
+    let keyinsert = ['res_count', 'fail_count', 'prompt_count', 'score'];
+    let keys = Object.keys(info);
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+      if (keyStr.includes(key)) {
+        str += `${key}='${info[key]}'`;
+      } else if (keyint.includes(key)) {
+        str += `${key}=${info[key]}`;
+      } else if (keyinsert.includes(key)) {
+        str += `${key}+=${info[key]}`;
+      }
+    }
+    console.log(str);
+    let sql = `
+      update challenge_record set ${str}
+      where challengeid = ${challengeid} and chuserid = ${uid}
+    `;
+    return queryFunc(sql);
+  },
 }
 
 module.exports = game;
--- router/game.js
+++ router/game.js
@@ -11,6 +11,13 @@ const {
   queryRankList,
   queryRankListAll,
   queryAppointmentGame,
+  queryAppointmentOtherGames,
+  queryAllGames,
+  updateGameAppointment,
+  insertChallengeRecord,
+  deleteChallengeRecord,
+  queryDoingGames,
+  updateChallengeRecord,
 } = require('../db/views/game');
 
 Game.get('/danlist', async ctx => {
@@ -46,7 +53,7 @@ Game.get('/ranklist/top3', async ctx => {
   if (Array.isArray(cacheRankList)) {
     return resBody(ctx, {
       message: '从缓存中读取',
-      data: cacheRankList.filter((item, i) => i < 3),
+      data: responseFormat(cacheRankList.filter((item, i) => i < 3)),
     })
   }
   cacheRankList = await queryRankListAll();
@@ -59,7 +66,7 @@ Game.get('/ranklist/all', async ctx => {
   if (Array.isArray(cacheRankList)) {
     return resBody(ctx, {
       message: '从缓存中读取',
-      data: cacheRankList,
+      data: responseFormat(cacheRankList),
     })
   }
   let { start, limit } = format(ctx.query);
@@ -69,13 +76,113 @@ Game.get('/ranklist/all', async ctx => {
   })
 })
 
+let gameAndJobMap = {};
 Game.get('/appointment', async ctx => {
   if (!tokenFailure(ctx.token, ctx)) return;
   let { start, limit } = format(ctx.query);
   let { uid } = ctx.info;
   let res = await queryAppointmentGame(uid, start, limit);
+  res.forEach(item => {
+    let { latetime, endtime, starttime, prompttime, challengeid } = item;
+    if (gameAndJobMap[challengeid]) return;
+    if (Date.now() - new Date(starttime).getTime() > 0) return;
+    let job = new schedule.scheduleJob(new Date(starttime), () => {
+      updateChallengeRecord(uid, challengeid, { status: 1 });
+      job = new schedule.scheduleJob(new Date(latetime), () => {
+        updateChallengeRecord(uid, challengeid, { status: 3 });
+        job = new schedule.scheduleJob(new Date(prompttime), () => {
+          // 发送通知
+          job = new schedule.scheduleJob(new Date(endtime), () => {
+            updateChallengeRecord(uid, challengeid, { status: 4 });
+            job.cancel();
+            delete gameAndJobMap[challengeid];
+          })
+        })
+      })
+    });
+    gameAndJobMap[challengeid] = job;
+  })
   return resBody(ctx, {
     message: '查询成功',
+    data: res,
+  })
+})
+
+Game.get('/all/list', async ctx => {
+  let { start, limit } = format(ctx.query);
+  let res = [];
+  if (!tokenFailure(ctx.token, ctx)) {
+    // 未登录，则获取所有的挑战赛
+    res = await queryAllGames(start, limit);
+  } else {
+    let { uid } = ctx.info;
+    res = await queryAppointmentOtherGames(uid, start, limit);
+  }
+  return resBody(ctx, {
+    message: '查询成功',
+    data: responseFormat(res),
+  })
+})
+
+Game.patch('/set/appointment', async ctx => {
+  if (!tokenFailure(ctx.token, ctx)) return;
+  let { num, rankid } = format(ctx.request.body);
+  let { uid } = ctx.info;
+  if (!rankid) {
+    return resBody(ctx, {
+      message: 'rankid是必须的',
+      status: 401,
+    })
+  }
+  let info = {};
+  if (num === 1) {
+    info = await insertChallengeRecord(rankid, uid);
+  } else if (num === -1) {
+    info = await deleteChallengeRecord(rankid, uid);
+  }
+  let { fieldCount } = info;
+  if (fieldCount === 0) {
+    let res = await updateGameAppointment(rankid, num);
+    return resBody(ctx, {
+      message: '操作成功',
+      data: res,
+    })
+  } else {
+    await num === 1 ? deleteChallengeRecord(rankid, uid) : insertChallengeRecord(rankid, uid);
+    return resBody(ctx, {
+      message: '服务器出现了一点小问题',
+      status: 401
+    })
+  }
+})
+
+Game.get('/doinglist', async ctx => {
+  if (!tokenFailure(ctx.token, ctx)) return;
+  let { uid } = ctx.info;
+  let res = await queryDoingGames(uid);
+  return resBody(ctx, {
+    message: '查询成功',
+    data: res,
+  })
+})
+
+Game.patch('/set/challenge_record', async ctx => {
+  if (!tokenFailure(ctx.token, ctx)) return;
+  let body = format(ctx.request.body);
+  let { challengeid } = body;
+  if (!challengeid) {
+    return resBody(ctx, {
+      status: 403,
+      message: 'challengeid是必须的'
+    })
+  }
+  let { uid } = ctx.info;
+  let res = null;
+  if (Object.keys(body).length > 1) {
+    // res = await updateChallengeRecord(uid, challengeid, body);
+  }
+  return resBody(ctx, {
+    message: '操作成功',
     data: res,
   })
 })
